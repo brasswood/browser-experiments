00381b9947b2eb2599408c2270224fa3c7daddad
Andrew Riachi <ariachi@ku.edu>
2025-07-22 16:26:15 -0500

    disable auto backups during run

--- mega.sh
+++ mega.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+systemctl --user disable backup.timer
+
 systemctl --user stop evolution-addressbook-factory.service
 systemctl --user stop evolution-calendar-factory.service
 systemctl --user stop evolution-source-registry.service
 systemctl --user stop evolution-user-prompter.service
 
-python -m experiments.browser_bench out/out_browser_bench
-python -m experiments.classic_with_samples out/out_classic_with_samples
+python -m experiments.browser_bench out/browser_bench
+python -m experiments out/classic
+
+systemctl --user enable backup.timer
