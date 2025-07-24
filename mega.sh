9eb8bf32ee56c9fc07ca5e90ce9b555e8eda7768
Andrew Riachi <ariachi@ku.edu>
2025-07-24 11:52:29 -0500

    stop and start onedrive service

--- mega.sh
+++ mega.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 systemctl --user disable backup.timer
-
+systemctl --user stop onedrive.service
 systemctl --user stop evolution-addressbook-factory.service
 systemctl --user stop evolution-calendar-factory.service
 systemctl --user stop evolution-source-registry.service
@@ -10,3 +10,4 @@ python -m experiments.browser_bench out/browser_bench
 python -m experiments out/classic
 
 systemctl --user enable backup.timer
+systemctl --user start onedrive.service
