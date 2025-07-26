6b969594118457eacf88017414e1842ece974807
Andrew Riachi <ariachi@ku.edu>
2025-07-26 18:49:56 -0500

    update browserbench experiments to use context manager

--- experiments/browser_bench/__init__.py
+++ experiments/browser_bench/__init__.py
@@ -40,48 +40,48 @@ EXPERIMENTS = [
 ]
 
 def main() -> None:
-    top_ctx = Context.from_module(__name__)
-    out_ctx = top_ctx.get_child("out")
-    for params in EXPERIMENTS:
-        assert params.name == "chromium" or params.name == "firefox"
-        start_button = lib.get_resource(f"start_button_{params.name}.png")
-        details_button = lib.get_resource(f"details_button_{params.name}.png")
-        copy_json_button = lib.get_resource(f"copy_json_button_{params.name}.png")
-        browser_ctx = out_ctx.get_child(params.name)
-        for (i, mem) in enumerate(params.mems):
-            mem_ctx = browser_ctx.get_child_with_mem(i, mem)
-            try:
-                lib.assert_not_running(params.name)
-                with mem_ctx.start_app(params.command):
-                    for j in range(SAMPLES):
-                        sample_ctx = mem_ctx.get_child_with_sample(j)
+    with Context.from_module(__name__) as top_ctx, top_ctx.get_child("out") as out_ctx:
+        for params in EXPERIMENTS:
+            assert params.name == "chromium" or params.name == "firefox"
+            start_button = lib.get_resource(f"start_button_{params.name}.png")
+            details_button = lib.get_resource(f"details_button_{params.name}.png")
+            copy_json_button = lib.get_resource(f"copy_json_button_{params.name}.png")
+            with out_ctx.get_child(params.name) as browser_ctx:
+                for (i, mem) in enumerate(params.mems):
+                    with browser_ctx.get_child_with_mem(i, mem) as mem_ctx:
                         try:
-                            point = lib.locate_center(start_button, timeout=10)
-                            with sample_ctx.monitor(params.name, check_if_running=False):
-                                start = time.time()
-                                pyautogui.click(*point)
-                                point = lib.locate_center(details_button, timeout=10*60)
-                                end = time.time()
-                            pyautogui.click(*point)
-                            point = lib.locate_center(copy_json_button, timeout=10)
-                            pyautogui.click(*point)
-                            with sample_ctx.open("benchmark.json", 'w') as f:
-                                f.write(pyperclip.paste())
-                            with sample_ctx.open("python_time_ms", 'w') as f:
-                                f.write(str((end - start) * 1000))
-                            lib.reload_page(params.name)
+                            lib.assert_not_running(params.name)
+                            with mem_ctx.start_app(params.command):
+                                for j in range(SAMPLES):
+                                    with mem_ctx.get_child_with_sample(j) as sample_ctx:
+                                        try:
+                                            point = lib.locate_center(start_button, timeout=10)
+                                            with sample_ctx.monitor(params.name, check_if_running=False):
+                                                start = time.time()
+                                                pyautogui.click(*point)
+                                                point = lib.locate_center(details_button, timeout=10*60)
+                                                end = time.time()
+                                            pyautogui.click(*point)
+                                            point = lib.locate_center(copy_json_button, timeout=10)
+                                            pyautogui.click(*point)
+                                            with sample_ctx.open("benchmark.json", 'w') as f:
+                                                f.write(pyperclip.paste())
+                                            with sample_ctx.open("python_time_ms", 'w') as f:
+                                                f.write(str((end - start) * 1000))
+                                            lib.reload_page(params.name)
+                                        except ImageNotFoundException:
+                                            # in this case, we just break to close the browser, because we
+                                            # are ordinarily leaving the browser open between runs so we're hopeless
+                                            # to get an accurate next measurement without waiting arbitrarily long.
+                                            sample_ctx.logger.error("Image not found, perhaps the application is too unresponsive. Refusing to reduce memory any more for this workload.", exc_info=True)
+                                            raise
+                                        except Exception as e:
+                                            sample_ctx.logger.exception(e)
+                        except TookLongTimeException as e:
+                            mem_ctx.logger.warning(f"Application took longer than {e.warn_time} seconds to exit. Refusing to reduce memory any more for this workload.")
+                            break
                         except ImageNotFoundException:
-                            # in this case, we just break to close the browser, because we
-                            # are ordinarily leaving the browser open between runs so we're hopeless
-                            # to get an accurate next measurement without waiting arbitrarily long.
-                            sample_ctx.logger.error("Image not found, perhaps the application is too unresponsive. Refusing to reduce memory any more for this workload.", exc_info=True)
-                            raise
+                            # same as ImageNotFoundException above; we may be thrashing so just end the experimentation here
+                            break
                         except Exception as e:
-                            sample_ctx.logger.exception(e)
-            except TookLongTimeException as e:
-                mem_ctx.logger.warning(f"Application took longer than {e.warn_time} seconds to exit. Refusing to reduce memory any more for this workload.")
-                break
-            except ImageNotFoundException:
-                break
-            except Exception as e:
-                mem_ctx.logger.exception(e)
+                            mem_ctx.logger.exception(e)
